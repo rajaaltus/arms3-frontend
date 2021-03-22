9ff94a4e2fb5cc26af2eeac8c0c92f5978e689f9
Raja s <rajaaltus@gmail.com>
2021-03-22 15:20:36 +0700

    datepicked fixed in super admin

--- components/SuperQuery.vue
+++ components/SuperQuery.vue
@@ -2,18 +2,7 @@
   <div>
     <v-row>
       <v-col cols="12" lg="2" class="mt-5">
-        <v-select
-          outlined
-          dense
-          v-model="selectedYear"
-          ref="year"
-          :items="reportYears"
-          item-value="id"
-          item-text="val"
-          label="Reporting Year"
-          placeholder="Pick Year"
-          color="success"
-        ></v-select>
+        <v-select outlined dense v-model="selectedYear" ref="year" :items="reportYears" item-value="id" item-text="val" label="Reporting Year" placeholder="Pick Year" color="success"></v-select>
       </v-col>
       <!-- <v-col cols="12" lg="3" class="mt-5">
             <v-menu
@@ -48,33 +37,21 @@
             </v-menu>
           </v-col> -->
       <v-col cols="12" lg="3">
-        <v-label><small>Start Date - End Date</small></v-label>
-        <vc-date-picker mode="range" v-model="range" ref="range" />
+        <vc-date-picker v-model="range" is-range style="display: flex; margin-top: 1.3rem; align-items: baseline; z-index: 99;">
+          <template v-slot="{ inputValue, inputEvents }">
+            <v-text-field :value="inputValue.start" label="From" v-on="inputEvents.start" outlined dense></v-text-field>
+            <svg style="width: 1rem; height: 1rem;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+            </svg>
+            <v-text-field :value="inputValue.end" label="To" v-on="inputEvents.end" outlined dense></v-text-field>
+          </template>
+        </vc-date-picker>
       </v-col>
-      <v-col
-        cols="12"
-        lg="2"
-        class="mt-5"
-        v-if="$auth.user.userType === 'DEPARTMENT' || $auth.user.userType==='SUPER_ADMIN'"
-      >
-        <v-select
-          outlined
-          dense
-          ref="user-type"
-          v-model="userType"
-          label="Report of the"
-          placeholder="Pick user type"
-          :items="userTypes"
-          color="success"
-        ></v-select>
+      <v-col cols="12" lg="2" class="mt-5" v-if="$auth.user.userType === 'DEPARTMENT' || $auth.user.userType === 'SUPER_ADMIN'">
+        <v-select outlined dense ref="user-type" v-model="userType" label="Report of the" placeholder="Pick user type" :items="userTypes" color="success"></v-select>
       </v-col>
 
-      <v-col
-        cols="12"
-        lg="3"
-        class="mt-5"
-        v-if="$auth.user.userType === 'DEPARTMENT' || $auth.user.userType==='SUPER_ADMIN'"
-      >
+      <v-col cols="12" lg="3" class="mt-5" v-if="$auth.user.userType === 'DEPARTMENT' || $auth.user.userType === 'SUPER_ADMIN'">
         <v-autocomplete
           outlined
           dense
@@ -96,21 +73,11 @@
             </template>
             <template v-else>
               <v-list-item-avatar>
-                <img
-                  :src="
-                    data.item.avatar !== null
-                      ? $axios.defaults.baseURL + data.item.avatar.url
-                      : '/avatar-default-icon.png'
-                  "
-                />
+                <img :src="data.item.avatar !== null ? $axios.defaults.baseURL + data.item.avatar.url : '/avatar-default-icon.png'" />
               </v-list-item-avatar>
               <v-list-item-content>
-                <v-list-item-title
-                  v-html="data.item.fullname"
-                ></v-list-item-title>
-                <v-list-item-subtitle
-                  v-html="data.item.userType"
-                ></v-list-item-subtitle>
+                <v-list-item-title v-html="data.item.fullname"></v-list-item-title>
+                <v-list-item-subtitle v-html="data.item.userType"></v-list-item-subtitle>
               </v-list-item-content>
             </template>
           </template>
@@ -120,29 +87,12 @@
       <v-col cols="auto" lg="auto">
         <v-row>
           <v-layout align-start justify-start>
-            <v-btn
-              v-if="selectedYear"
-              :loading="loading"
-              :disabled="loading"
-              color="green"
-              x-small
-              class="mt-6 mr-1 white--text"
-              fab
-              @click="loader()"
-            >
+            <v-btn v-if="selectedYear" :loading="loading" :disabled="loading" color="green" x-small class="mt-6 mr-1 white--text" fab @click="loader()">
               Go
             </v-btn>
             <v-tooltip right color="blue-grey darken-2">
               <template v-slot:activator="{ on }">
-                <v-btn
-                  color="blue-grey"
-                  fab
-                  x-small
-                  class="mt-6 white--text"
-                  dark
-                  @click="resetFilter"
-                  v-on="on"
-                >
+                <v-btn color="blue-grey" fab x-small class="mt-6 white--text" dark @click="resetFilter" v-on="on">
                   <v-icon>mdi-reload</v-icon>
                 </v-btn>
               </template>
@@ -153,66 +103,63 @@
       </v-col>
     </v-row>
     <div>
-    <!-- <pre>{{query}}</pre> -->
+      <!-- <pre>{{query}}</pre> -->
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['reportYears', 'userTypes'],
+  props: ["reportYears", "userTypes"],
   data() {
     return {
       report: 1,
       range: {
-        start: '',
-        end: '',
+        start: "",
+        end: "",
       },
-      menu: '',
+      menu: "",
       sheet: false,
       dataLoaded: false,
       assignedPeople: [],
       loading: false,
-      selectedUser: '',
+      selectedUser: "",
       selectedYear: 0,
-      selectedQuery: '',
+      selectedQuery: "",
       query: {
-        year: '',
-        range: '',
-        userType: '',
-        selectedUser: ''
+        year: "",
+        range: "",
+        userType: "",
+        selectedUser: "",
       },
-      yearParam: '',
-      userTypeParam: '',
-      monthParam: '',
-      userParam: '',
-      userType: '',
-    }
+      yearParam: "",
+      userTypeParam: "",
+      monthParam: "",
+      userParam: "",
+      userType: "",
+    };
   },
   watch: {
     // report(val) {
     //   this.reportStepper = val;
     // },
     selectedYear(val) {
-      if(val) {
+      if (val) {
         this.yearParam = "annual_year=" + val;
-        this.query.year=this.yearParam;
+        this.query.year = this.yearParam;
       }
-      
     },
     range(val) {
-      console.log('Range Val: ', val);
-      if(val) {
-       var range = Object.assign({}, val);
-      this.monthParam = `&created_at_gt=${this.$moment(range.start).format(
-        "YYYY-MM-DD"
-      )}&created_at_lt=${this.$moment(range.end).format("YYYY-MM-DD")}`;
+      console.log("Range Val: ", val);
+      if (val) {
+        var range = Object.assign({}, val);
+        this.monthParam = `&created_at_gt=${this.$moment(range.start).format("YYYY-MM-DD")}&created_at_lt=${this.$moment(range.end).format("YYYY-MM-DD")}`;
       }
       this.query.range = this.monthParam;
     },
     userType(val) {
-      this.userParam = '';
-      if(val) {
+      this.userParam = "";
+      if (val) {
         this.userTypeParam = `&user.userType=${val}`;
         this.query.userType = this.userTypeParam;
         if (val === "FACULTY") this.assignedPeople = this.faculties;
@@ -221,13 +168,12 @@ export default {
       }
     },
     selectedUser(val) {
-      if(val) {
-        this.query.userType = '';
+      if (val) {
+        this.query.userType = "";
         this.userParam = `&user.id=${val}`;
         this.query.selectedUser = this.userParam;
       }
     },
-    
   },
   computed: {
     people() {
@@ -241,36 +187,38 @@ export default {
     },
   },
   async mounted() {
-    let querySelector = '';
-    querySelector = 'department.id='+`${this.$auth.user.department}`+'&blocked_ne=true';
-    this.$store.dispatch('user/setActiveUsersList', {qs: querySelector});
+    let querySelector = "";
+    querySelector = "department.id=" + `${this.$auth.user.department}` + "&blocked_ne=true";
+    this.$store.dispatch("user/setActiveUsersList", { qs: querySelector });
   },
   methods: {
     loader() {
       this.selectedQuery = this.query.year + this.query.range + this.query.userType + this.query.selectedUser;
-      this.$emit('go', this.selectedQuery, this.selectedYear, this.range)
+      this.$emit("go", this.selectedQuery, this.selectedYear, this.range);
       console.log("Selected Query: ", this.selectedQuery);
     },
     resetFilter() {
-     
       this.$emit("resetFilters");
       this.range = null;
-      this.selectedQuery = '';
+      this.selectedQuery = "";
       this.selectedYear = 0;
-      this.userType = '';
-      this.yearParam = '';
-      this.monthParam = '';
-      this.userTypeParam = '';
-      this.userParam = '';
-      this.query = Object.assign({}, {
-        year: '',
-        range: '',
-        userType: '',
-        selectedUser: ''
-      });
-      this.selectedUser = '';
+      this.userType = "";
+      this.yearParam = "";
+      this.monthParam = "";
+      this.userTypeParam = "";
+      this.userParam = "";
+      this.query = Object.assign(
+        {},
+        {
+          year: "",
+          range: "",
+          userType: "",
+          selectedUser: "",
+        }
+      );
+      this.selectedUser = "";
       this.assignedPeople = this.people;
-    }
-  }
-}
+    },
+  },
+};
 </script>
