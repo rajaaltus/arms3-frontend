0c4f66f53e517718260267d3554bd31db2d3bd82
robinson87 <a.robinsonsilvester@gmail.com>
2021-04-20 14:55:55 +0530

    About formatting

--- components/Fomatting/AboutDataFormat.vue
+++ components/Fomatting/AboutDataFormat.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <center>
+    <!-- <center>
       <h2>NATIONAL INSTITUTE OF MENTAL HEALTH &amp; NEUROSCIENCES</h2>
       <h3>Bengaluru – 560029</h3>
       <h2>Period of the report:1st April {{ $store.state.selectedYear }} to 31st March {{ $store.state.selectedYear + 1 }}</h2>
-    </center>
-    <h1>
+    </center> -->
+    <h2>
       <b><u>Section A:</u></b>
-    </h1>
-    <h3>1. ABOUT THE DEPARTMENT</h3>
-    <h3>A. Introduction: Specifically indicate the recognition / contribution of the Department during the year to policies, planning and programmes at State / National and International levels.</h3>
+    </h2>
+    <h4>1. ABOUT THE DEPARTMENT</h4>
+    <h4>A. Introduction: Specifically indicate the recognition / contribution of the Department during the year to policies, planning and programmes at State / National and International levels.</h4>
     <p>{{ aboutData.introduction }}</p>
-    <h3>B. New facilities developed: New initiatives taken up by the Department(s) within NIMHANS during the year.</h3>
+    <h4>B. New facilities developed: New initiatives taken up by the Department(s) within NIMHANS during the year.</h4>
     <p>{{ aboutData.facilities }}</p>
   </div>
 </template>
